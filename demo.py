404e763d3d79df7efa88f117c5375a5a7d3e0193
sonalloya <sonaloya2003@gmail.com>
2025-12-24 11:11:48 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,12 +1,6 @@
 def add_numbers(a, b):
-    """This function adds two numbers and 
-    prints the result.
-
-    Parameters:
-        a (int): The first number to add
-        b (int): The second number to add
-    """
+   
     result = a + b;
     print("Sum is: " + str(result)
 
-add_numbers(5, 10)
+add_numbers(5 10);
